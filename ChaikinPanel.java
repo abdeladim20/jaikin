c88fdc6887801dd70781ae6414e8ace3916af85b
abdeladim20 <linuxacount1@gmail.com>
2025-10-05 09:54:12 +0100

    Implement Chaikin algorithm function

--- ChaikinPanel.java
+++ ChaikinPanel.java
@@ -1,8 +1,9 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import javax.swing.*;
 
 public class ChaikinPanel extends JPanel {
 
@@ -15,6 +16,30 @@ public class ChaikinPanel extends JPanel {
         setPreferredSize(new Dimension(800, 600));
         setBackground(Color.BLACK);
         setFocusable(true);
+    }
+
+    private static List<Point2D.Float> chaikin(List<Point2D.Float> points) {
+        if (points.size() < 2) {
+            return new ArrayList<>(points);
+        }
+
+        List<Point2D.Float> newPoints = new ArrayList<>();
+        newPoints.add(points.get(0));
+
+        for (int i = 0; i < points.size() - 1; i++) {
+            Point2D.Float p0 = points.get(i);
+            Point2D.Float p1 = points.get(i + 1);
+            
+            float qx = p0.x + (p1.x - p0.x) * 0.25f;
+            float qy = p0.y + (p1.y - p0.y) * 0.25f;
+            float rx = p0.x + (p1.x - p0.x) * 0.75f;
+            float ry = p0.y + (p1.y - p0.y) * 0.75f;
+
+            newPoints.add(new Point2D.Float(qx, qy));
+            newPoints.add(new Point2D.Float(rx, ry));
+        }
+        newPoints.add(points.get(points.size() - 1));
+        return newPoints;
     }
 
     @Override
